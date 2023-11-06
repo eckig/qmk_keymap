4f92a28bd3ed66ee9254639d7a8fe54dfd5b4a95
eckig <eckig@users.noreply.github.com>
2023-11-06 17:08:36 +0100

    .

--- eckig/config.h
+++ eckig/config.h
@@ -3,8 +3,11 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #define ORYX_CONFIGURATOR
+#undef RGB_DISABLE_TIMEOUT
+#define RGB_DISABLE_TIMEOUT 60000
+
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define FIRMWARE_VERSION u8"XYMA4/NLGJb"
+#define FIRMWARE_VERSION u8"XYMA4/3dYjw"
 #define RAW_USAGE_PAGE 0xFF60
 #define RAW_USAGE_ID 0x61
 #define LAYER_STATE_8BIT
