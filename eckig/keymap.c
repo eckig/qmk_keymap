e787c03cdcf76eaf0fb9718ee897390cc4b6d9b3
eckig <eckig@users.noreply.github.com>
2023-11-13 14:37:18 +0100

    .

--- eckig/keymap.c
+++ eckig/keymap.c
@@ -25,9 +25,9 @@ uint8_t NUM_CUSTOM_SHIFT_KEYS = sizeof(custom_shift_keys) / sizeof(custom_shift_
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
-    MT(MOD_LALT, DE_DLR),    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_LGUI,                                           KC_PSCR,  KC_TRANSPARENT,  KC_TRANSPARENT, KC_TRANSPARENT, ALT_TAB, MT(MOD_RALT,DE_EXLM),
+    DE_DLR,                  KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_LGUI,                                           KC_LALT,  KC_PSCR,         KC_TRANSPARENT, KC_TRANSPARENT, ALT_TAB, DE_EXLM,
     MT(MOD_LGUI, KC_TAB),    DE_SCLN,  KC_COMM,  KC_DOT,   KC_P,     DE_Y,                                              KC_F,     KC_G,            KC_C,           KC_R,           KC_L,    DE_SLSH,
-    MT(MOD_LCTL, KC_ESCAPE), KC_A,     KC_O,     KC_E,     KC_U,     KC_I,                                              KC_D,     KC_H,            KC_T,           KC_N,           KC_S,    MT(MOD_RCTL,DE_MINS),
+    MT(MOD_LCTL, KC_ESCAPE), KC_A,     KC_O,     KC_E,     KC_U,     KC_I,                                              KC_D,     KC_H,            KC_T,           KC_N,           KC_S,    MT(MOD_RCTL, DE_MINS),
     KC_LSFT,                 DE_QUOT,  KC_Q,     KC_J,     KC_K,     KC_X,                                              KC_B,     KC_M,            KC_W,           KC_V,           DE_Z,    KC_RSFT,
                                                     LT(2,KC_BSPC), LT(1,KC_DELETE),                                 LT(1,KC_ENTER),       LT(2,KC_SPACE)
   ),
