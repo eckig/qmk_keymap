2c4deb6156a767bf792b4475434dc9be9b9e3f83
eckig <eckig@users.noreply.github.com>
2023-11-16 20:39:56 +0100

    .

--- eckig/keymap.c
+++ eckig/keymap.c
@@ -143,18 +143,17 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 
     HSV hsv = {0, 255, 255};
     if (layer == 0) {
-        hsv.r = 205;
+        hsv.h = 205;
         hsv.s = 10;
         hsv.v = 99;
     }
     else if (layer == 1) {
-        hsv.r = 125;
+        hsv.h = 125;
         hsv.s = 5;
         hsv.v = 96;
     }
     else if (layer == 2) {
-        hsv = {37, 12, 100};
-        hsv.r = 37;
+        hsv.h = 37;
         hsv.s = 12;
         hsv.v = 100;
     }
