8f8abff606c6896b893312243c7b73fef19069fc
eckig <eckig@users.noreply.github.com>
2023-11-16 19:47:44 +0100

    .

--- eckig/keymap.c
+++ eckig/keymap.c
@@ -147,17 +147,23 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 
                 if (index >= led_min && index < led_max && index != NO_LED && keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
 
+                  HSV hsv = {0, 255, 255};
                   switch (layer) {
                     case 0:
-                      rgblight_sethsv_at(HSV_SPRINGGREEN, index);
+                      hsv = {205, 10, 99};
                       break;
                     case 1:
-                      rgblight_sethsv_at(HSV_TEAL, index);
+                      hsv = {125, 5, 96};
                       break;
                     case 2:
-                      rgblight_sethsv_at(HSV_WHITE, index);
+                      hsv = {37, 12, 100};
                       break;
                   }
+                  if (hsv.v > rgb_matrix_get_val()) {
+                      hsv.v = rgb_matrix_get_val();
+                  }
+                  RGB rgb = hsv_to_rgb(hsv);
+                  rgb_matrix_set_color(index, rgb.r, rgb.g, rgb.b);
                 }
             }
         }
