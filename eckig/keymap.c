0909bb7f94d0117a26151542cc76d704cd37c180
eckig <eckig@users.noreply.github.com>
2024-03-18 14:05:15 +0100

    .

--- eckig/keymap.c
+++ eckig/keymap.c
@@ -36,16 +36,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT_voyager(
     KC_F1,                   KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,                        KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
     KC_TRANSPARENT,          KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                        KC_NO,    KC_7,    KC_8,    KC_9,    KC_NO,   KC_TRANSPARENT,
-    KC_TRANSPARENT,          DE_ADIA,  DE_ODIA,  KC_NO,    DE_UDIA,  KC_LEFT,                      KC_NO,    KC_4,    KC_5,    KC_6,    DE_SS,   KC_TRANSPARENT,
-    KC_TRANSPARENT,          KC_NO,    KC_NO,    KC_DOWN,  KC_UP,    KC_RIGHT,                     KC_NO,    KC_1,    KC_2,    KC_3,    KC_NO,   KC_TRANSPARENT,
-                                                    KC_TRANSPARENT, KC_TRANSPARENT,             KC_TRANSPARENT, KC_0
+    KC_TRANSPARENT,          DE_ADIA,  DE_ODIA,  KC_NO,    DE_UDIA,  KC_NO,                        KC_NO,    KC_4,    KC_5,    KC_6,    DE_SS,   KC_TRANSPARENT,
+    KC_TRANSPARENT,          KC_NO,    KC_NO,    KC_DOWN,  KC_UP,    KC_NO,                        KC_NO,    KC_1,    KC_2,    KC_3,    KC_NO,   KC_TRANSPARENT,
+                                                    KC_LEFT,        KC_RIGHT,                   KC_TRANSPARENT, KC_0
   ),
   [2] = LAYOUT_voyager(
-    RGB_TOG,                 KC_NO,    KC_NO,    KC_NO,    RGB_VAD,  RGB_VAI,                      KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_PGUP, KC_NO,
-    KC_TRANSPARENT,          DE_DEG,   KC_NO,    KC_NO,    KC_NO,    DE_GRV,                       DE_AMPR,  DE_LBRC, DE_RBRC, DE_EURO, KC_PGDN, KC_TRANSPARENT,
-    KC_TRANSPARENT,          DE_CIRC,  DE_BSLS,  DE_PLUS,  DE_EQL,   DE_HASH,                      DE_PIPE,  DE_LPRN, DE_RPRN, DE_PERC, KC_HOME, KC_TRANSPARENT,
-    KC_TRANSPARENT,          DE_SECT,  KC_NO,    DE_ASTR,  KC_NO,    KC_NO,                        DE_TILD,  DE_LCBR, DE_RCBR, DE_AT,   KC_END,  KC_TRANSPARENT,
-                                                    KC_TRANSPARENT, KC_TRANSPARENT,             KC_TRANSPARENT, KC_TRANSPARENT
+    RGB_TOG,                 KC_NO,    KC_NO,    KC_NO,    RGB_VAD,  RGB_VAI,                      KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    KC_TRANSPARENT,          DE_DEG,   KC_NO,    KC_NO,    KC_NO,    DE_GRV,                       DE_AMPR,  DE_LBRC, DE_RBRC, DE_EURO, KC_NO,   KC_TRANSPARENT,
+    KC_TRANSPARENT,          DE_CIRC,  DE_BSLS,  DE_PLUS,  DE_EQL,   DE_HASH,                      DE_PIPE,  DE_LPRN, DE_RPRN, DE_PERC, KC_NO,   KC_TRANSPARENT,
+    KC_TRANSPARENT,          DE_SECT,  KC_NO,    DE_ASTR,  KC_NO,    KC_NO,                        DE_TILD,  DE_LCBR, DE_RCBR, DE_AT,   KC_NO,   KC_TRANSPARENT,
+                                                    KC_TRANSPARENT, KC_TRANSPARENT,             KC_HOME,        KC_END
   ),
 };
 
