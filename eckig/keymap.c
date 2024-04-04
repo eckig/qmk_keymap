6ddfd8a462013eb7fb52272139e3847f35604f26
eckig <eckig@users.noreply.github.com>
2024-04-04 16:07:14 +0200

    .

--- eckig/keymap.c
+++ eckig/keymap.c
@@ -14,10 +14,10 @@ enum custom_keycodes {
   MT_EXLM = SAFE_RANGE + 2,
 };
 
-#define LT2_SPACE  LT(2, KC_SPACE)
-#define LT2_BSPC   LT(2, KC_BSPC)
-#define LT1_E      LT(1, KC_E)
-#define LT1_T      LT(1, KC_T)
+#define LT1_ENTER  LT(1, KC_ENTER)
+#define LT1_DELETE LT(1, KC_DELETE)
+#define LT2_E      LT(2, KC_E)
+#define LT2_T      LT(2, KC_T)
 #define MT_CTL_ESC MT(MOD_LCTL, KC_ESCAPE)
 #define MT_CTL_MIN MT(MOD_RCTL, DE_MINS)
 #define MT_ALT_DLR MT(MOD_LALT, MT_DLR)
@@ -38,9 +38,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
     MT_ALT_DLR, KC_F5,   KC_F6,   KC_F7,   KC_F8,      KC_NO,                 KC_PSCR, KC_NO,      KC_NO,   KC_NO,   ALT_TAB, MT_ALT_EXC,
     KC_TAB,     DE_SCLN, KC_COMM, KC_DOT,  KC_P,       DE_Y,                  KC_F,    KC_G,       KC_C,    KC_R,    KC_L,    DE_SLSH,
-    MT_CTL_ESC, KC_A,    KC_O,    LT1_E,   MT_SHIFT_U, KC_I,                  KC_D,    MT_SHIFT_H, LT1_T,   KC_N,    KC_S,    MT_CTL_MIN,
+    MT_CTL_ESC, KC_A,    KC_O,    LT2_E,   MT_SHIFT_U, KC_I,                  KC_D,    MT_SHIFT_H, LT2_T,   KC_N,    KC_S,    MT_CTL_MIN,
     KC_LGUI,    DE_QUOT, KC_Q,    KC_J,    KC_K,       KC_X,                  KC_B,    KC_M,       KC_W,    KC_V,    DE_Z,    KC_RGUI,
-                                                 LT2_BSPC, KC_DELETE,                  KC_ENTER,  LT2_SPACE
+                                                 KC_BSPC,  LT1_DELETE,                 LT1_ENTER, KC_SPACE
   ),
   [1] = LAYOUT_voyager(
     KC_F1,      KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,                 KC_F7,   KC_F8,      KC_F9,   KC_F10,  KC_F11,  KC_F12,
@@ -57,7 +57,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                  KC_LEFT,  KC_RIGHT,                   KC_HOME,   KC_END
   ),
 };
-
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
@@ -107,8 +106,8 @@ void matrix_scan_user(void) {
 uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t* record) {
   // If you quickly hold a tap-hold key after tapping it, the tap action is repeated. 
   switch (keycode) {
-    case LT2_SPACE:
-    case LT2_BSPC:
+    case LT1_ENTER:
+    case LT1_DELETE:
       return QUICK_TAP_TERM;  // Enable key repeating.
     default:
       return 0;  // Otherwise, force hold and disable key repeating.
@@ -143,10 +142,13 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         if (keycode <= KC_TRNS) {
           rgb_matrix_set_color(index, 0, 0, 0);
         }
-        else if(IS_QK_MOD_TAP(keycode) || keycode == LT1_E || keycode == LT1_T) {
+        else if(IS_QK_MOD_TAP(keycode) || keycode == LT2_E || keycode == LT2_T) {
           rgb_matrix_set_color(index, f * 255, f * 136, f * 0);
         }
-        else if(keycode == LT2_SPACE || keycode == LT2_BSPC || layer == 2) {
+        else if(keycode == LT1_ENTER || keycode == LT1_DELETE) {
+          rgb_matrix_set_color(index, 0, f * 255, f * 127);
+        }
+        else if(layer == 2) {
           rgb_matrix_set_color(index, 0, f * 128, f * 128);
         }
         else if(layer == 0) {
